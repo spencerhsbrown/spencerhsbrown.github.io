65830e3f4705a686f65a825bc6a8fb30386abca2
Sponger <spencerhsbrown@gmail.com>
2024-10-14 21:14:26 -0600

    ray should be marching

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -21,3 +21,33 @@ controls.enableDamping = true;
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(1, 1, 1);
 scene.add(light);
+
+
+//add screen plane
+const geometry = new THREE.PlaneGeometry();
+const material = new THREE.ShaderMaterial();
+const rayMarchPlane = new THREE.Mesh(geometry, material);
+
+// width and height of plane
+const nearPlaneWidth = camera.near * Math.tan(THREE.MathUtils.degToRad(camera.fov / 2)) * camera.aspect * 2;
+const nearPlaneHeight = nearPlaneWidth / camera.aspect;
+rayMarchPlane.scale.set(nearPlaneWidth, nearPlaneHeight, 1);
+
+scene.add(rayMarchPlane)
+
+let cameraForwardPos = new THREE.Vector3(0, 0, -1);
+const VECTOR3ZERO = new THREE.Vector3(0, 0, 0);
+
+let time = Date.now();
+
+const animate = () => {
+    requestAnimationFrame(animate)
+
+    cameraForwardPos = camera.position.clone().add(camera.getWorldDirection(VECTOR3ZERO).multiplyScalar(camera.near));
+    rayMarchPlane.position.copy(cameraForwardPos);
+    rayMarchPlane.rotation.copy(camera.rotation);
+
+    renderer.render(scene, camera);
+
+    controls.update();
+}
