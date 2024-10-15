735c17e6a4cd8d5bc119550aa33fc28c87fea798
Sponger <spencerhsbrown@gmail.com>
2024-10-15 03:28:35 -0600

    finished it maybe idk

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -1,3 +1,5 @@
+// following https://medium.com/@nabilnymansour/ray-marching-in-three-js-66b03e3a6af2
+
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
@@ -49,6 +51,159 @@ const animate = () => {
 
     renderer.render(scene, camera);
 
+    uniforms.u_time.value = (Date.now() - time) / 1000;
+
     controls.update();
 }
 animate();
+
+
+const vertCode = `
+out vec2 vUv;
+
+void main() {
+    vec4 worldPos = modelViewMatrix * vec4(position, 1.0);
+    vec3 viewDir = normalize(-worldPos.xyz);
+
+    gl_Position = projectionMatrix * worldPos;
+
+    vUv = uv;
+}
+`;
+
+const fragCode = `
+
+// Uniforms
+const uniforms = {
+    u_eps: { value: 0.001 },
+    u_maxDis: { value: 1000 },
+    u_maxSteps: { value: 100 },
+
+    u_clearColor: { value: backgroundColor },
+
+    u_camPos: { value: camera.position },
+    u_camToWorldMat: { value: camera.matrixWorld },
+    u_camInvProjMat: { value: camera.projectionMatrixInverse },
+
+    u_lightDir: { value: light.position },
+    u_lightColor: { value: light.color },
+
+    u_diffIntesity: { value: 0.5 },
+    u_specIntensity: { value: 3 },
+    u_ambientIntesity: { value: 0.15 },
+    u_shininess: { value: 16 },
+
+    u_time: { value: 0 },
+};
+material.uniforms = uniforms;
+
+precision mediump float:
+
+in vec2 vUv;
+
+uniform vec3 u_clearColor;
+
+uniform float u_eps;
+uniform float u_maxDis;
+uniform int u_maxSteps;
+
+uniform vec3 u_camPos;
+uniform mat4 u_camToWorldMat;
+uniform mat4 u_camInvProjMat;
+
+uniform vec3 u_lightDir;
+uniform vec3 u_lightColor;
+
+uniform float u_diffIntesity;
+uniform float u_specIntensity;
+uniform float u_ambientIntesity;
+uniform float u_shininess;
+
+uniform float u_time;
+
+
+float smin(float a, float b, float k) {
+    float h = clamp(0.5 + 0.5 * (b - a) / k, 0.0, 1.0);
+    return mix(b, a, h) - k * h * (1.0 - h);
+}
+
+float scene(vec3 p){
+
+    float sphere1Dis = distance(p, vec3(cos(u_time), sin(u_time), 0)) - 1.;
+
+    float sphere2Dis = distance(p, vec3(cos(u_time), sin(u_time), 0)) - 0.75;
+
+    return smin(sphere1Dis, sphere2Dis, 0.5);
+}
+
+float rayMarch(vec3 ro, vec3 rd)
+{
+    float d = 0.;
+    float cd;
+    vec3 p;
+
+    for (int i = 0; i < u_maxSteps; ++i) {
+        p = ro + d * rd;
+        cd = scene(p);
+
+        if (cd < u_eps || d >= u_maxDis) break;
+
+        d += cd;
+    }
+
+    return d;
+}
+
+vec3 sceneCol(vec3 p){
+    float sphere1Dis = distance(p, vec3(cos(u_time), sin(u_time), 0)) - 1.;
+    float sphere2Dis = distance(p, vec3(cos(u_time), sin(u_time), 0)) - 0.75;
+
+    float k = 0.5;
+    float h = clamp(0.5 + 0.5 * (sphere2Dis - sphere1Dis) / k, 0.0, 1.0);
+
+    vec3 color1 = vec3(1, 0, 0);
+    vec3 color2 = vec3(0, 0, 1);
+
+    return mix(color1, color2, h);
+}
+
+vec3 normal(vec3 p) // from https://iquilezles.org/articles/normalsSDF/
+{
+    vec3 n = vec3(0, 0, 0);
+    vec3 e;
+    for (int i = 0; i < 4; i++) {
+        e = 0.5773 * (2.0 * vec3((((i + 3) >> 1) & 1), ((i >> 1) & 1), (i & 1)) - 1.0);
+        n += e * scene(p + e * u_eps);
+    }
+    return normalize(n);
+}
+
+void main() {
+
+    vec2 uv = vUv.xy;
+
+    vec3 ro = u_camPos;
+    vec3 rd = (u_camInvProjMat * vec4(uv * 2. - 1., 0, 1)).xyz;
+    rd = (u_camToWorldMat * vec4(rd, 0)).xyz;
+    rd = normalize(rd);
+
+    float disTravelled = rayMarch(ro, rd);
+
+    vec3 hp = ro + disTravelled * rd;
+
+    vec 3 n = normal(hp);
+
+    if (disTravelled >= u_maxDis) {
+        gl_FragColor = vec4(u_clearColor, 1);
+    } else {
+        float dotNL = dot(n, u_lightDir, 1);
+        float diff = max(dotNL, 0.0) * u_diffIntesity;
+        float spec = pow(diff, u_shininess) * u_specIntensity;
+        float ambient = u_ambientIntesity;
+
+        vec3 color = u_lightColor * (sceneCol(hp) * (spec + ambient + diff));
+        gl_FragColor = vec4(color, 1);
+
+    }
+}
+`
