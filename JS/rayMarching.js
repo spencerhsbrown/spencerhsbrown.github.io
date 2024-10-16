934e352cd71fd1953168bdd929c5f48a9a959246
Sponger <spencerhsbrown@gmail.com>
2024-10-16 03:06:02 -0600

    dsa

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -109,10 +109,8 @@ float smin(float a, float b, float k) {
 float scene(vec3 p){
 
     float sphere1Dis = distance(p, vec3(0,0,0)) - 1.;
-
-    float sphere2Dis = distance(p, vec3(sin(u_time), cos(u_time), 0)) - 0.5;
-
-    return smin(sphere1Dis, sphere2Dis, 0.75);
+    return sphere1Dis;
+    //return smin(sphere1Dis, sphere2Dis, 0.75);
 }
 
 float rayMarch(vec3 ro, vec3 rd)
