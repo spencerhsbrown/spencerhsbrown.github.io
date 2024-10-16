b9804c85564a6d0db0484ccf5d144530210ad43b
Sponger <spencerhsbrown@gmail.com>
2024-10-16 13:51:29 -0600

    testing colors

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -148,7 +148,7 @@ vec3 sceneCol(vec3 p){
     float boxDis = sdBox(p, vec3(1,1,1));
 
     float k = 0.5;
-    float h = clamp(0.5 + 0.5 * (sphere1Dis-boxDis) / k, 0.0, 1.0);
+    float h = clamp(0.5 + 0.5 * (boxDis-sphere1Dis) / k, 0.0, 1.0);
 
     vec3 color1 = vec3(0, 1, 0);
     vec3 color2 = vec3(1,0,0);
