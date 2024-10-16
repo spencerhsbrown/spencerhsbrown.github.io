46b175a19dafd3eaec8521ef595bef7d81160ec4
Sponger <spencerhsbrown@gmail.com>
2024-10-16 17:19:24 -0600

    ..

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -116,7 +116,7 @@ float scene(vec3 p){
 
     float boxDis = sdBox(p, vec3(1,1,1));
 
-    float sphere1Dis = distance(p,vec3(sin(u_time)*2,0,0)) - 1.;
+    float sphere1Dis = distance(p,vec3(sin(u_time*2),0,0)) - 1.;
 
     //return sphere1Dis;
 
@@ -146,7 +146,7 @@ float rayMarch(vec3 ro, vec3 rd)
 vec3 sceneCol(vec3 p){
     float boxDis = sdBox(p, vec3(1,1,1));
 
-    float sphere1Dis = distance(p,vec3(0,sin(u_time)*2,0)) - 1.;
+    float sphere1Dis = distance(p,vec3(0,sin(u_time*2),0)) - 1.;
 
     float colormix = smin(sphere1Dis, boxDis, 0.5);
 
