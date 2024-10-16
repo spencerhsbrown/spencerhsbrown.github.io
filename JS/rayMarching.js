d3e87a0358b54bf079b46f1ebf4eda2f801895f0
Sponger <spencerhsbrown@gmail.com>
2024-10-16 14:37:25 -0600

    testing, all goes one color when combining. i think smin is doing something im not wanting

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -116,7 +116,7 @@ float scene(vec3 p){
 
     float boxDis = sdBox(p, vec3(1,1,1));
 
-    float sphere1Dis = distance(p,vec3(sin(u_time),cos(u_time),0)) - 1.;
+    float sphere1Dis = distance(p,vec3(sin(u_time),cos(u_time*1.5),0)) - 1.;
 
     //return sphere1Dis;
 
@@ -147,7 +147,7 @@ vec3 sceneCol(vec3 p){
     float sphere1Dis = distance(p,vec3(sin(u_time),cos(u_time),0)) - 1.;
     float boxDis = sdBox(p, vec3(1,1,1));
 
-    float colormix = smin(sphere1Dis, boxDis, 0.25);
+    float colormix = smin(sphere1Dis, boxDis, 0.5);
 
     vec3 color1 = vec3(0, 1, 0);
     vec3 color2 = vec3(1,0,0);
