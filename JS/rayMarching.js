362ce66b83adf28fc332fbd154e49ef3b0d9de9c
Sponger <spencerhsbrown@gmail.com>
2024-10-16 17:32:58 -0600

    messing with color again

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -148,7 +148,7 @@ vec3 sceneCol(vec3 p){
 
     float sphere1Dis = distance(p,vec3(0,sin(u_time)*2.0,0)) - 1.;
 
-    float colormix = smin(sphere1Dis, boxDis, 0.5);
+    float colormix = smin(sphere1Dis, boxDis, 0.7);
 
     vec3 color1 = vec3(0, 1, 0);
     vec3 color2 = vec3(1,0,0);
