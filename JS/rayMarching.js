3688f8b58d6e2902ed1693ad5756e777f77011cc
Sponger <spencerhsbrown@gmail.com>
2024-10-17 21:39:35 -0600

    testing silly things

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -108,8 +108,7 @@ float smin(float a, float b, float k) {
 
 float sdBox( vec3 p, vec3 b )
 {
-  vec3 q = abs(p) - b;
-  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+  return length(p.xz-c.xy)-c.z;
 }
 
 float scene(vec3 p){
