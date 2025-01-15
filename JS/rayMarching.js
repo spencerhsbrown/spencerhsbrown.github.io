b44749b42b5245cb0be22c736aea195040175bf0
Sponger <spencerhsbrown@gmail.com>
2025-01-15 13:05:49 -0700

    might break

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -110,28 +110,6 @@ float sphere(vec3 currentPosition, vec3 center, float radius) {
     return distance(currentPosition, center) - radius;
 }
 
-float opLimitedRepetitionColor(vec3 p, vec3 s, vec3 l, float radius, out float height)
-{
-    
-    bool calculateHeight = (height != -1.0);
-
-     // Calculate the grid index
-    vec3 gridIndex = clamp(floor(p / s + 0.5), -l, l);
-
-    // Calculate the animated offset
-    //sin(gridIndex.x + gridIndex.z + (u_time*3.0)) * 0.2
-
-    
-    vec3 offset = vec3(0.0, sin(gridIndex.x + gridIndex.z + (u_time*3.0)) * 0.2, 0.0);
-
-    // Calculate the position of the current sphere
-    vec3 offsetSpheres = p-s*gridIndex - offset;
-    
-    height = offsetSpheres.y;
-
-    // Return the SDF of the sphere at this position
-    return sphere(offsetSpheres, vec3(0.0), radius);
-}
 float opLimitedRepetition(vec3 p, vec3 s, vec3 l, float radius)
 {
      // Calculate the grid index
@@ -177,25 +155,19 @@ float rayMarch(vec3 rayOrigin, vec3 rayDirection)
     return totalDistance;
 }
 
-vec3 getColorByHeight(float height, float minHeight, float maxHeight)
+vec3 getColorByHeight(vec3 currentPosition)
 {
-    float t = clamp((height-minHeight) / (maxHeight - minHeight), 0.0, 1.0);
+    float height = mod(currentPosition.y, 1.0); // Map height to [0, 1]
 
     vec3 color1 = vec3(1.0, 0.0, 0.0);
     vec3 color2 = vec3(0.0, 1.0, 0.0);
 
-    return mix(color1, color2, t);
+    return mix(color1, color2, height);
 }   
 
 vec3 sceneCol(vec3 currentPosition){
-    
-    vec3 boundingBox = vec3(1.0, 5.0, 1.0);
-    vec3 gridSize = vec3(30.0 ,0.0 ,30.0);
-
-    float height;
-    float spheres = opLimitedRepetitionColor(currentPosition, boundingBox, gridSize, 0.25, height);
-
-    return getColorByHeight(height, -1.5, 1.5);
+   
+    return getColorByHeight(currentPosition);
 }
 
 vec3 normal(vec3 p) // from https://iquilezles.org/articles/normalsSDF/
