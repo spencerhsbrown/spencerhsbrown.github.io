caa463682fa5c4ebcf7e2972dfb7166b2739bf87
Sponger <spencerhsbrown@gmail.com>
2025-01-15 20:33:59 -0700

    tesdft

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -42,7 +42,7 @@ rayMarchPlane.scale.set(nearPlaneWidth, nearPlaneHeight, 1);
 const uniforms = {
     u_epsilonValue: { value: 0.001 },
     u_maxDis: { value: 1000 },
-    u_maxSteps: { value: 100 },
+    u_maxSteps: { value: 1000 },
 
     u_clearColor: { value: backgroundColor },
 
