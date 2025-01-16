71ddb9870e31ca8f3acdb2ad9504566555b44561
Sponger <spencerhsbrown@gmail.com>
2025-01-15 20:27:22 -0700

    asdfghjkl;

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -129,7 +129,7 @@ float opLimitedRepetition(vec3 p, vec3 s, vec3 l, float radius)
     return sphere(offsetSpheres, vec3(0.0), radius);
 }
 float scene(vec3 currentPosition){
-    vec3 boundingBox = vec3(1.0, 1.0, 1.0);
+    vec3 boundingBox = vec3(2.0, 1.0, 2.0);
     vec3 gridSize = vec3(30.0 ,0.0 ,30.0);
 
     float spheres = opLimitedRepetition(currentPosition, boundingBox, gridSize, 0.25);
