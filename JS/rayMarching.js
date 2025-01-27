14ff72b3e3782e6c2433ef8112e1a83877055613
Sponger <spencerhsbrown@gmail.com>
2025-01-27 12:07:21 -0700

    testing what is making things look strange

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -79,8 +79,8 @@ const uniforms = {
 
     u_diffIntesity: { value: 0.2 },
     u_specIntensity: { value: 0.2 },
-    u_ambientIntesity: { value: 0.4 },
-    u_shininess: { value: 0.0 },
+    u_ambientIntesity: { value: 0.0 },
+    u_shininess: { value: 0.1 },
 
     u_time: { value: 0 },
 
