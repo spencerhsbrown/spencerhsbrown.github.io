47aa9983311ff7a513d6886f017eac48e9db0a34
Sponger <spencerhsbrown@gmail.com>
2025-01-27 02:47:37 -0700

    dumb

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -146,7 +146,7 @@ float sphere(vec3 currentPosition, vec3 center, float radius) {
 float scene(vec3 currentPosition){
 
     float torus = sdTorus(currentPosition, vec2(1.0,0.2));
-    float spheres = sphere(currentPosition, vec3(0.0, u_spherePosition, 0.0), 0.5\3);
+    float spheres = sphere(currentPosition, vec3(0.0, u_spherePosition, 0.0), 0.3);
 
     return smin(torus,spheres, u_epsilonValue);
 }
