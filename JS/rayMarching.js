661808202bc931875dc4d5ff16a2b6de0b3fbe02
Sponger <spencerhsbrown@gmail.com>
2025-01-29 23:07:35 -0700

    is slider value techiinically a float?

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -12,6 +12,7 @@ output.innerHTML = slider.value; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function () {
     output.innerHTML = this.value;
+    var shinyslider = this.value;
 } 
 
 
