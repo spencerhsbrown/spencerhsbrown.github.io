a13ec4aecebf547bfffc9c4f502b56b47c7fe079
Sponger <spencerhsbrown@gmail.com>
2025-01-29 22:14:40 -0700

    might break a lot of things idk yet

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -265,3 +265,13 @@ const animate = () => {
     uniforms.u_spherePosition.value = spherePosition;
 }
 animate();
+
+//slider Functionality
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    output.innerHTML = this.value;
+} 
