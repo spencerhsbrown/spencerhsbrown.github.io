c7879a7d8bdc4253c19b38cb0d37a3a63b136b20
Sponger <spencerhsbrown@gmail.com>
2025-01-29 22:23:53 -0700

    i should lookup if javascript cares where this goes

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -4,6 +4,16 @@ import * as THREE from 'https://esm.sh/three';
 import { OrbitControls } from "https://esm.sh/three/examples/jsm/controls/OrbitControls";;
 
 
+//slider Functionality
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    output.innerHTML = this.value;
+} 
+
 
 const scene = new THREE.Scene();
 
@@ -265,13 +275,3 @@ const animate = () => {
     uniforms.u_spherePosition.value = spherePosition;
 }
 animate();
-
-//slider Functionality
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
-    output.innerHTML = this.value;
-} 
