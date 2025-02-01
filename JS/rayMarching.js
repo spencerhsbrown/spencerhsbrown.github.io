ebea7148066817f8a88802cce4c25ebb4d281764
Sponger <spencerhsbrown@gmail.com>
2025-01-31 21:04:33 -0700

    bug fixes

--- JS/rayMarching.js
+++ JS/rayMarching.js
@@ -32,8 +32,6 @@ const sliders = [
     new SliderControl("maxStepsRange", "maxStepsTextValue", "u_maxSteps")
 ];
 
-maxSteps
-
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
