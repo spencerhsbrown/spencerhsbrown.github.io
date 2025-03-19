68ece74ce57bac300a8cb458bd2670c367474653
Sponger <spencerhsbrown@gmail.com>
2025-03-19 16:35:07 -0600

    why no cube

--- JS/threejs.js
+++ JS/threejs.js
@@ -1,12 +1,12 @@
 import * as THREE from 'three';
 
-
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
 
 //objects
 const geometry = new THREE.SphereGeometry(15, 32, 16);
