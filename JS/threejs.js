4da1751dbaa316ada109a972d5b318e24543aae8
Sponger <spencerhsbrown@gmail.com>
2025-03-19 16:44:50 -0600

    CUBE CHANGE SPHEERE IDK

--- JS/threejs.js
+++ JS/threejs.js
@@ -14,7 +14,7 @@ document.body.appendChild(renderer.domElement);
 //const sphere = new THREE.Mesh(geometry, material);
 //scene.add(sphere);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.SphereGeometry(15, 32, 16);;
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
